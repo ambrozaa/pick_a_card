e5bc17adfdc2b136158e840019c0173ac36d7fa9
Анна <anna@MacBook-Air-MacBook.local>
2024-02-01 16:43:54 +0300

    Title

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,5 @@
 # encoding: UTF-8
+puts "Pick a Card"
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ["\u2660", "\u2665", "\u2666", "\u2663"] # Буби, черви, крести, пики.
 puts "#{values.sample} #{suits.sample.to_s}"
